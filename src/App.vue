a7160ace7c130d8d9e09d913a6048b3bbced6d6c
elisabichiappini <elisabichiappini@gmail.com>
2023-12-06 23:50:45 +0100

    feat: bonus

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <script>
+import HeroComponent from './components/HeroComponent.vue';
 export default {
-  name: 'AppTitle',
-  data() {
-    return {
-      title: 'La mia prima App con Vite',
-    }
-  },
+    name: 'AppTitle',
+    data() {
+        return {
+            title: 'La mia prima App con Vite',
+        };
+    },
+    compontents: {
+        HeroComponent,
+    },
+    components: { HeroComponent }
 }
 </script>
 
 <template>
+  <HeroComponent/>
   <h1>{{ title }}</h1>
 </template>
 
---
+++ src/components/HeroComponent.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'Jumbotron',
+    data() {
+        return {
+            title: 'Ciao sono keyup :)',
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <h2>{{ title }}</h2>
+    </header>
+</template>
